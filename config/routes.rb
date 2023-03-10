c7b6761524d29939d46df708014112bbf03049bd
heyextra <extrafahrali@gmail.com>
2023-03-10 12:33:20 -0500

    web font / static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: { sessions: 'admins/sessions' }
   root "items#index"
+  get "/pages/:page" => "pages#show"
 
+  
   resources :items, :categories
 end
