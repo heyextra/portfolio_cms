2d088c1c52d69a77f8752f5d0a447c8a42c8e6d6
heyextra <extrafahrali@gmail.com>
2023-03-11 02:11:54 -0500

    Added categories check boxes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,6 +42,6 @@ class ItemsController < ApplicationController
   end
   private
     def item_params
-      params.require(:item).permit(:title, :description, :link, :tags, :admin_id)
+      params.require(:item).permit(:title, :description, :link, :tags, :admin_id, :category_ids => [])
     end
 end
